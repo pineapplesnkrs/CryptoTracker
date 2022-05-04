00804556b117684fd697cb70b32889a0307c558b
12smithc <smithc12@students.rowan.edu>
2022-05-04 00:09:50 -0400

    Update userSchema.js
    user Schema made

---
+++ models/userSchema.js
@@ -0,0 +1,26 @@
+let mongoose = require('mongoose');
+
+
+const userSchema = new mongoose.Schema({
+	_id: {
+		type: String,
+		required: [true, 'We need to know your name']
+    },
+	username: {
+        type: String,
+        required: [true, 'must have a username']
+    },
+	password:{
+        type: String,
+        required: [true, 'must have password']
+    },
+	email:{
+		type: String,
+		required: [true, 'You must enter an email']
+	},
+    favorites: Array,
+
+});
+const userCol=mongoose.model('User', userSchema)
+
+module.exports = userCol;
